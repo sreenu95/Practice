ce3754f1a6fbd959b146e385101659f6a23352fa
Mallikarjuna <Mallikarjuna@DESKTOP-AJKT6UV>
2020-05-13 11:58:39 +0530

    new project for polymorphism

---
+++ OOPSExamples11/src/com/oops/polymorphism/OverLoading.java
@@ -0,0 +1,24 @@
+package com.oops.polymorphism;
+class Add
+{
+	public static void  sum(int a, int b)
+	{
+		System.out.println("sum is"+(a+b));
+	}
+}
+class Add1 extends Add
+{
+	public static void sum(int a,int b,int c)
+	{
+		System.out.println("sum of 3 numbers is"+(a+b+c));
+	}
+}
+public class OverLoading {
+
+	public static void main(String[] args) {
+		Add1 a=new Add1();
+		a.sum(12, 4);
+		a.sum(98, 4, 3);
+	}
+
+}
